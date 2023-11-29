cbf6b252dc5c7b45504bbaf3f7e5b4b602922e2a
Emma Gardner <egardner2019@gmail.com>
2023-11-28 23:14:26 -0500

    get nouns and verbs from historical features

--- helperMethods.js
+++ helperMethods.js
@@ -1,14 +1,85 @@
+import readXlsxFile, { readSheetNames } from "read-excel-file/node";
+import WordPOS from "wordpos";
+
+const wordpos = new WordPOS();
+
 /**
- *
- * @param {string} dataSet The name of the data set being used
+ * Get an array containing arrays for the features of each release
+ * @param {"Zoom" | "Webex" | "Discord"} dataSet The data set being used
+ * @returns An array of arrays, where each sub-array contains the features for the sheet at that index + 1
  */
-const getNounsVerbs = (dataSet) => {
-  // REMEMBER THAT THE DATASETS ARE REVERSED. LATEST IS AT THE TOP!
-  // Get the number of available releases (numReleases)
-  // Get the data from all but the last 3 releases (first 3 since it's reversed)
-  // Create an array to hold all nouns and verbs (nounVerbArray)
-  // For each feature in each release, use wordpos-web to get all of the nouns and verbs. Add them to nounVerbArray.
-  // Return nounVerbArray with the duplicate values removed
+const getFeaturesFromFile = async (dataSet) => {
+  const filePath = `data/${dataSet}.xlsx`;
+
+  // Get the names of the sheets in the .xlsx file
+  const sheetNames = await readSheetNames(filePath);
+
+  // A sub-method to get the features from a specific sheet
+  const getFeaturesFromSheet = async (sheetName) => {
+    let thisSheetsFeatures = [];
+
+    const rows = await readXlsxFile(filePath, { sheet: sheetName });
+    rows.forEach((rowArray, index) => {
+      // Don't include the first row (it's just a title)
+      if (index !== 0) {
+        thisSheetsFeatures.push(rowArray[0]);
+      }
+    });
+
+    return thisSheetsFeatures;
+  };
+
+  // Get all of the features
+  const allFeaturesByRelease = await Promise.all(
+    sheetNames.map(async (sheetName) => await getFeaturesFromSheet(sheetName))
+  );
+
+  // Remove the first array since it contains the number of features for each release, not the textual descriptions of the features
+  allFeaturesByRelease.shift();
+
+  // Return the array of arrays containing the features by release
+  return allFeaturesByRelease;
 };
 
-export { getNounsVerbs };
+/**
+ * Get all of the nouns and verbs of the features included in the given 2-dimensional array
+ * @param {*} allFeaturesByRelease The array containing arrays with the textual descriptions of the features of the associated release
+ * @returns A 1-dimensional array of all the unique nouns and verbs (lowercased) in all of the given features
+ */
+const getNounsVerbs = async (allFeaturesByRelease) => {
+  // A sub-method to get the nouns and verbs of all the features of a given release
+  const getNounsVerbsOfRelease = async (releaseArray) => {
+    const nounsAndVerbsOfReleaseArray = await Promise.all(
+      // For each feature in this release, get the nouns and verbs as an array of words
+      releaseArray.map(async (feature) => {
+        const theseNouns = await wordpos.getNouns(feature);
+        const theseVerbs = await wordpos.getVerbs(feature);
+
+        // Return an array containing the nouns and verbs in this feature description
+        return theseNouns.concat(theseVerbs);
+      })
+    );
+
+    // Return a 1-dimensional array containing all of the nouns and verbs in the features of this release
+    return nounsAndVerbsOfReleaseArray.flat(Infinity);
+  };
+
+  // Get all of the nouns and verbs in the historical release features
+  let allNounsAndVerbs = await Promise.all(
+    allFeaturesByRelease.map(
+      async (thisReleasesFeatures) =>
+        await getNounsVerbsOfRelease(thisReleasesFeatures)
+    )
+  );
+
+  // Make the array 1-dimensional
+  allNounsAndVerbs = allNounsAndVerbs.flat(Infinity);
+
+  // Make all words lowercased
+  allNounsAndVerbs = allNounsAndVerbs.map((nounVerb) => nounVerb.toLowerCase());
+
+  // Return the nouns and verbs array with duplicated removed
+  return Array.from(new Set(allNounsAndVerbs));
+};
+
+export { getNounsVerbs, getFeaturesFromFile };
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-import { getNounsVerbs } from "./helperMethods.js";
+import { getFeaturesFromFile, getNounsVerbs } from "./helperMethods.js";
 
-const mainLogic = (dataSet) => {
-  // 1. Read in the historical requirements and create a collection of all the nouns and verbs (use wordpos-web)
-    const allNounsAndVerbs = getNounsVerbs();
-    console.log(allNounsAndVerbs);
-  // Ask the user for the desired number of requirements for the next iteration
-  // If the desired number of requirements is less than or equal to the number of available requirements, alert the user and quit the program
-  // 2. For the number of requirements the user wants, ... (for loop)
-  // 2a. Calculate the reward for selecting each term in the historical vocabulary (how often it appears in the historical documents)
-  // 2b. Select a term from the historical vocabulary based on the calculated rewards
-  // 2c. Calculate the reward for selecting each requirement based on the average textual similarity score when comparing to each of the original historical documents containing that term (use sentence-similarity)
-  // 2d. Select a requirement based on the calculated reward
-  // 2e. Add the selected requirement to the list of requirements to be implemented in the next iteration
-  // Remove the selected requirement from the list of available requirements, and decrease the reward of the selected term by 25%
+// The number of requirements desired for the next iteration
+const numReqs = 20;
+
+const mainLogic = async (dataSet) => {
+  // Get the reviews from the .xlsx file (array of arrays, with each subarray designated to a release)
+  const allFeaturesByRelease = await getFeaturesFromFile(dataSet);
+
+  // 1. Create an array containing all of the unique nouns and verbs (lowercased) in the historical releases
+  // Used slice to exclude the summary sheet and the 3 latest releases, which will be used for evaluation
+  const allNounsAndVerbs = await getNounsVerbs(allFeaturesByRelease.slice(4));
+
+  // 2. For the number of requirements desired (numReqs), ...
+  for (let i = 1; i <= numReqs; i++) {
+    // 2a. Calculate the reward for selecting each term in the historical vocabulary (how often it appears in the historical documents)
+    // 2b. Select a term from the historical vocabulary based on the calculated rewards
+    // 2c. Calculate the reward for selecting each requirement based on the average textual similarity score when comparing to each of the original historical documents containing that term (use sentence-similarity)
+    // 2d. Select a requirement based on the calculated reward
+    // 2e. Add the selected requirement to the list of requirements to be implemented in the next iteration
+    // Remove the selected requirement from the list of available requirements, and decrease the reward of the selected term by 25%
+  }
   // 3. Print the selected and unselected requirements to separate files
   // 4. Evaluate the effectiveness of this method (accuracy, precision, recall)
 };
 
 mainLogic("Zoom");
-mainLogic("Webex");
-mainLogic("Discord");
+// mainLogic("Webex");
+// mainLogic("Discord");
